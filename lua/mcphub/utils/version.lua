091ca8aacc9148b5204ba9a40ed94d9807e40307
ravitemer <ravitemer@gmail.com>
2025-04-21 16:43:59 +0000

    feat: replace $ENV_VAR in command args for stdio servers from "env" config (fixes #98)
    Config like below should work.
    
    ```json
    {
      "mcpServers": {
        "postgres": {
          "command": "docker",
          "args": [
            "run",
            "-i",
            "--rm",
            "-e",
            "POSTGRES_URL",
            "mcp/postgres",
            "$POSTGRES_URL"
          ],
          "env": {
            "POSTGRES_URL": null
          }
        }
      }
    }
    ```

--- lua/mcphub/utils/version.lua
+++ lua/mcphub/utils/version.lua
@@ -3,7 +3,7 @@ return {
     REQUIRED_NODE_VERSION = { -- Required mcp-hub version
         major = 3,
         minor = 0,
-        patch = 4,
-        string = "3.0.4",
+        patch = 5,
+        string = "3.0.5",
     },
 }
