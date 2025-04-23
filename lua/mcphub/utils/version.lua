e7456548c564d20163e58f1d9e91022296c4abb4
ravitemer <ravitemer@gmail.com>
2025-04-23 13:51:46 +0000

    fix: false positive modified triggers for servers with env field with falsy values

--- lua/mcphub/utils/version.lua
+++ lua/mcphub/utils/version.lua
@@ -3,7 +3,7 @@ return {
     REQUIRED_NODE_VERSION = { -- Required mcp-hub version
         major = 3,
         minor = 1,
-        patch = 0,
-        string = "3.1.0",
+        patch = 2,
+        string = "3.1.2",
     },
 }
