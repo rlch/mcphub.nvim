d1fa57e51568d97ba63f0bb34789d4331720440e
ravitemer <ravitemer@gmail.com>
2025-04-25 13:31:41 +0000

    chore: update mcp-hub version

--- lua/mcphub/utils/version.lua
+++ lua/mcphub/utils/version.lua
@@ -3,7 +3,7 @@ return {
     REQUIRED_NODE_VERSION = { -- Required mcp-hub version
         major = 3,
         minor = 1,
-        patch = 5,
-        string = "3.1.5",
+        patch = 6,
+        string = "3.1.6",
     },
 }
