8e44cc9cf2785aeab07172981e158e27a4b5321f
ravitemer <ravitemer@gmail.com>
2025-05-03 12:17:24 +0000

    fix: version number

--- lua/mcphub/utils/version.lua
+++ lua/mcphub/utils/version.lua
@@ -3,7 +3,7 @@ return {
     REQUIRED_NODE_VERSION = { -- Required mcp-hub version
         major = 3,
         minor = 1,
-        patch = 6,
+        patch = 7,
         string = "3.1.7",
     },
 }
